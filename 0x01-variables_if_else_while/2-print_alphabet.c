b86e3ce7821e4273cac410dd4604bcee410c1f25
William Dogbey <williamapepe@gmail.com>
2022-09-10 13:38:02 -0700

    correc the script

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- *main - Entry point
- *Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 
 int main(void)
 {
-char ch = 'a';
-while
-(ch <= 'z');
-{
-putchar(ch);
-ch++;
+	int x;
+
+for (x = 'a'; x <= 'z'; x++)
+	{
+putchar(x);
 }
 putchar('\n');
 return (0);
