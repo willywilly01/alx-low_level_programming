e6bb6b7092b5eb24d2d58cd3fef3110f08e1aee6
William Dogbey <williamapepe@gmail.com>
2022-09-10 17:21:04 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - print lowercase alpha a-z but remove 'q' and 'e'
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n = 0;
+	while
+	(n < 10);
+	{
+	printf("%d\n", n);
+	}
+n++;
+return (0);
+}
