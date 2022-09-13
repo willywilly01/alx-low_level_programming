b6dac70beaade5e5e596a34c477b5074d29c1563
William Dogbey <williamapepe@gmail.com>
2022-09-13 12:41:45 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+int n;
+char c;
+for (n = 0; n <=10; n++)
+{
+	for (c ='a'; c<='z'; c++)
+	{
+		_putchar(c);
+	}
+
+_putchar('\n');
+}
