775460889f076c8266790d4543de5088a24f86b7
William Dogbey <williamapepe@gmail.com>
2022-09-13 12:47:46 -0700

    print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,16 @@
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
+{
 int n;
 char c;
-for (n = 0; n <=10; n++)
+for (n = 0; n <= 10; n++)
 {
-	for (c ='a'; c<='z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
 
 _putchar('\n');
+}
 }
