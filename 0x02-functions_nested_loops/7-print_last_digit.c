ea79c2ca1bfb15aaf10619c57a852974e3b65be1
William Dogbey <williamapepe@gmail.com>
2022-09-14 10:02:50 -0700

    print hours and minutes available to Jack

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,24 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * print_last_digit - return last digit
- * @n : number to check
- * Return:0 or 1
+ * jack_bauer - function that prints every minute of the day, 24 hour clock
+ * h = hour, m = minutes
+ * / 10 allows second digit to rotate
+ * for loop breaks before passing 24:00
+ * Return: 24 hour clock line by line
  */
-int print_last_digit(int n)
+void jack_bauer(void)
 {
-
-int last_digit = n % 10;
-
-if (last_digit < 0)
-
-last_digit *= -1;
-
-_putchar(last_digit + '0');
-
-return (last_digit);
-
+int h, m;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+}
+}
 }
